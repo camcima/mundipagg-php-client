f1d9df60a90114a371b6f4e606e129befd2107cb
Carlos Cima <camcima@hotmail.com>
2013-04-26 14:52:43 -0300

    getters and setters

--- src/MundiPagg/Entity/ManageOrderResult.php
+++ src/MundiPagg/Entity/ManageOrderResult.php
@@ -9,6 +9,7 @@ namespace MundiPagg\Entity;
  */
 class ManageOrderResult extends AbstractEntity
 {
+
     /**
      * Coleção de CreditCardTransactionResult que contém
      * os resultados das operações que foram solicitadas no Request.
@@ -99,4 +100,135 @@ class ManageOrderResult extends AbstractEntity
      */
     public $Version;
 
+    public function getCreditCardTransactionResultCollection()
+    {
+        return $this->CreditCardTransactionResultCollection;
+    }
+
+    public function setCreditCardTransactionResultCollection($CreditCardTransactionResultCollection)
+    {
+        $this->CreditCardTransactionResultCollection = $CreditCardTransactionResultCollection;
+        return $this;
+    }
+
+    public function getBoletoTransactionResultCollection()
+    {
+        return $this->BoletoTransactionResultCollection;
+    }
+
+    public function setBoletoTransactionResultCollection($BoletoTransactionResultCollection)
+    {
+        $this->BoletoTransactionResultCollection = $BoletoTransactionResultCollection;
+        return $this;
+    }
+
+    public function getMundiPaggSuggestion()
+    {
+        return $this->MundiPaggSuggestion;
+    }
+
+    public function setMundiPaggSuggestion($MundiPaggSuggestion)
+    {
+        $this->MundiPaggSuggestion = $MundiPaggSuggestion;
+        return $this;
+    }
+
+    public function getErrorReport()
+    {
+        return $this->ErrorReport;
+    }
+
+    public function setErrorReport(\MundiPagg\Entity\ErrorReport $ErrorReport)
+    {
+        $this->ErrorReport = $ErrorReport;
+        return $this;
+    }
+
+    public function getRequestKey()
+    {
+        return $this->RequestKey;
+    }
+
+    public function setRequestKey($RequestKey)
+    {
+        $this->RequestKey = $RequestKey;
+        return $this;
+    }
+
+    public function getOrderKey()
+    {
+        return $this->OrderKey;
+    }
+
+    public function setOrderKey($OrderKey)
+    {
+        $this->OrderKey = $OrderKey;
+        return $this;
+    }
+
+    public function getOrderReference()
+    {
+        return $this->OrderReference;
+    }
+
+    public function setOrderReference($OrderReference)
+    {
+        $this->OrderReference = $OrderReference;
+        return $this;
+    }
+
+    public function getSuccess()
+    {
+        return $this->Success;
+    }
+
+    public function setSuccess($Success)
+    {
+        $this->Success = $Success;
+        return $this;
+    }
+
+    public function getManageOrderOperationEnum()
+    {
+        return $this->ManageOrderOperationEnum;
+    }
+
+    public function setManageOrderOperationEnum(\MundiPagg\Entity\Enum\ManageOrderOperationEnum $ManageOrderOperationEnum)
+    {
+        $this->ManageOrderOperationEnum = $ManageOrderOperationEnum;
+        return $this;
+    }
+
+    public function getMundiPaggTimeInMilliseconds()
+    {
+        return $this->MundiPaggTimeInMilliseconds;
+    }
+
+    public function setMundiPaggTimeInMilliseconds($MundiPaggTimeInMilliseconds)
+    {
+        $this->MundiPaggTimeInMilliseconds = $MundiPaggTimeInMilliseconds;
+        return $this;
+    }
+
+    public function getOrderStatusEnum()
+    {
+        return $this->OrderStatusEnum;
+    }
+
+    public function setOrderStatusEnum($OrderStatusEnum)
+    {
+        $this->OrderStatusEnum = $OrderStatusEnum;
+        return $this;
+    }
+
+    public function getVersion()
+    {
+        return $this->Version;
+    }
+
+    public function setVersion($Version)
+    {
+        $this->Version = $Version;
+        return $this;
+    }
 }
