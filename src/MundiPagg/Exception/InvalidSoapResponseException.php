c6552a4c312019ba685d9506261ab34e164b688f
Carlos Cima <camcima@hotmail.com>
2013-05-10 12:14:32 -0300

    forgot to add this file...

---
+++ src/MundiPagg/Exception/InvalidSoapResponseException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace MundiPagg\Exception;
+
+/**
+ * Invalid SOAP Response Exception
+ *
+ * @author Carlos Cima
+ */
+class InvalidSoapResponseException extends \Exception
+{
+    
+}
